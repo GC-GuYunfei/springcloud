a4be96fd71a15948af02d6185ca03cf2b9d4b42c
jiangmh <minghong.jiang@grandchallenge.co.jp>
2021-07-12 16:42:49 +0800

    Add donfig 半自动更新

--- cloud-config-client3355/src/main/java/com/jiangfendou/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/jiangfendou/springcloud/controller/ConfigClientController.java
@@ -1,11 +1,13 @@
 package com.jiangfendou.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/config-client")
 public class ConfigClientController {
 
@@ -16,4 +18,5 @@ public class ConfigClientController {
     public String getConfigInfo() {
         return configInfo;
     }
+
 }
