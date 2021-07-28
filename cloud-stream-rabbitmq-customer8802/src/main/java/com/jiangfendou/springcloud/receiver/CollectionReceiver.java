6500d8338e8473270b786c7a979a6b3cdb74fd3c
jiangmh <minghong.jiang@grandchallenge.co.jp>
2021-07-28 12:18:50 +0800

    Update some code

--- cloud-stream-rabbitmq-customer8802/src/main/java/com/jiangfendou/springcloud/receiver/CollectionReceiver.java
+++ cloud-stream-rabbitmq-customer8802/src/main/java/com/jiangfendou/springcloud/receiver/CollectionReceiver.java
@@ -6,7 +6,6 @@ import com.jiangfendou.springcloud.entiy.CollectionRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.stereotype.Component;
 
 /**
  * @author jiangmh
